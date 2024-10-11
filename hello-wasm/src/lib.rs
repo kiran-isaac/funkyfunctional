87e9f213fa86dc104db09104931162770812f6e3
Kiran <kiranisturt@outlook.com>
2024-10-11 15:26:45 +0100

    wasm proof of concept

--- hello-wasm/src/lib.rs
+++ hello-wasm/src/lib.rs
@@ -11,3 +11,13 @@ extern "C" {
 pub fn greet() {
     alert("Hello, hello-wasm!");
 }
+
+#[wasm_bindgen]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[wasm_bindgen]
+pub fn hello_world() -> String {
+    "Hello from Rust!".to_string()
+}
