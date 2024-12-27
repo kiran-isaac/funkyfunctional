58139a8036ee54f21c02322c5dc0728c34c3a493
Kiran <kiranisturt@outlook.com>
2024-12-27 21:36:12 +0000

    types to string

--- sfl_lib/src/types.rs
+++ sfl_lib/src/types.rs
@@ -25,3 +25,68 @@ impl std::fmt::Debug for TypeError {
         write!(f, "Type Error at [{}:{}]: {}", self.line, self.col, self.e)
     }
 }
+
+impl ToString for Type {
+    fn to_string(&self) -> String {
+        match self {
+            Type::Primitive(p) => match p {
+                Primitive::Int64 => "Int".to_string(),
+                Primitive::Float64 => "Float".to_string(),
+                _ => unimplemented!(),
+            },
+            Type::Function(t1, t2) => {
+                let t1_string = &t1.to_string();
+                let t1_string = match t1.as_ref() {
+                    Type::Function(_, _) => format!("({})", t1_string),
+                    _ => t1_string.clone(),
+                };
+
+                let t2_string = &t2.to_string();
+                // let t2_string = match t2.as_ref() {
+                //     Type::Function(_, _) => format!("({})", t2_string),
+                //     _ => t2_string.clone(),
+                // };
+
+                format!("{} -> {}", t1_string, t2_string)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn type_to_string() {
+        let t1 = Type::Primitive(Primitive::Int64);
+        assert_eq!(t1.to_string(), "Int");
+
+        let t2 = Type::Primitive(Primitive::Float64);
+        assert_eq!(t2.to_string(), "Float");
+
+        let t3 = Type::Function(
+            Box::new(Type::Primitive(Primitive::Int64)),
+            Box::new(Type::Primitive(Primitive::Float64)),
+        );
+        assert_eq!(t3.to_string(), "Int -> Float");
+
+        let t4 = Type::Function(
+            Box::new(Type::Primitive(Primitive::Int64)),
+            Box::new(Type::Function(
+                Box::new(Type::Primitive(Primitive::Int64)),
+                Box::new(Type::Primitive(Primitive::Float64)),
+            )),
+        );
+        assert_eq!(t4.to_string(), "Int -> Int -> Float");
+
+        let t5 = Type::Function(
+            Box::new(Type::Function(
+                Box::new(Type::Primitive(Primitive::Int64)),
+                Box::new(Type::Primitive(Primitive::Int64)),
+            )),
+            Box::new(Type::Primitive(Primitive::Float64)),
+        );
+        assert_eq!(t5.to_string(), "(Int -> Int) -> Float");
+    }
+}
