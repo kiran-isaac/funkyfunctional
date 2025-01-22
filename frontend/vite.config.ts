53af00b5d1bd0201a394b13a4ac594e7f35e3607
Kiran <kiranisturt@outlook.com>
2025-01-22 19:46:50 +0000

    switch back to my domain

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "http://kiran-isaac.github.io/funkyfunctional/",
+  base: "http://redexplorer.kiransturt.co.uk",
   server: {
     fs: {
       strict: false
