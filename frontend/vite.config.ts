40734b6423c7a4966ccb242ea8dcc4813a30063d
Kiran <kiranisturt@outlook.com>
2025-01-22 19:35:16 +0000

    change vite base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://redexplorer.kiransturt.co.uk",
+  base: "http://kiran-isaac.github.io/funkyfunctional/",
   server: {
     fs: {
       strict: false
