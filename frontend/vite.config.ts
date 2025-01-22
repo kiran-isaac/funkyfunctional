78256e34163ca7910dc173eca48f6ef0b38a9e54
Kiran <kiranisturt@outlook.com>
2025-01-22 19:25:21 +0000

    fix vite serving config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, searchForWorkspaceRoot } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -6,11 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     fs: {
-      allow: [
-        searchForWorkspaceRoot(process.cwd()),
-        // all wasm
-        process.cwd() + "/../wasm_lib/pkg/sfl_wasm_lib_bg.wasm",
-      ],
+      strict: false
     },
   },
 })
