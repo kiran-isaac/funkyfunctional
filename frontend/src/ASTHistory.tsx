cd21363e00df8c8cc06525abd367d21cb2400645
Kiran <kiranisturt@outlook.com>
2025-01-29 15:07:22 +0000

    Revert "stuff"
    This reverts commit 37eb26f04b77fb333d31130da92e6cc05b3f606c.

--- frontend/src/ASTHistory.tsx
+++ frontend/src/ASTHistory.tsx
@@ -6,18 +6,6 @@ interface ASTHistoryProps {
     rcToHistory: string[];
 }
 
-function colourizeMatches(string: string, toMatch: string, noMatchClassName: string, matchClassName: string , keyPrefix: string): JSX.Element[] {
-    const list: JSX.Element[] = [];
-    const parts = string.split(toMatch);
-    for (let j = 0; j < parts.length; j++) {
-        list.push(<span className={noMatchClassName} key={`${keyPrefix}-${j}-nomatch`}>{parts[j]}</span>);
-        if (j < parts.length - 1) {
-            list.push(<span className={matchClassName} key={`${keyPrefix}-${j}-match`}>{toMatch}</span>);
-        }
-    }
-    return list;
-}
-
 const ASTHistory = ({ astHistory, rcFromHistory, rcToHistory }: ASTHistoryProps) => {
     const astStrings = [];
     for (let i = 0; i < astHistory.length; i++) {
@@ -32,50 +20,38 @@ const ASTHistory = ({ astHistory, rcFromHistory, rcToHistory }: ASTHistoryProps)
     console.log(astStrings, rcFromHistory, rcToHistory);
 
     const astLIs = [];
-    for (let i = astStrings.length - 1; i > 0; i--) {
-        let list: JSX.Element[] = [];
-        const prev_to_this = rcToHistory[i - 1];
-        console.log(i);
-        
-        // const next_from = rcFromHistory[i];
+    for (let i = astStrings.length - 1; i >= 0; i--) {
+        const list: JSX.Element[] = [];
+        const rc_to = rcToHistory[i - 1];
+        const next_from = rcFromHistory[i];
 
-        let next_from_this = "";
-        if (i < rcFromHistory.length - 1) {
-            next_from_this = rcFromHistory[i + 1];
-        }
+        // let next_from = "";
+        // if (i < rcFromHistory.length - 1) {
+        //     next_from = rcFromHistory[i + 1];
+        // }
         const current = astStrings[i];
 
         // Get all occurences of rc_from in current, and make them bold
         if (i == astStrings.length - 1) {
-            list = colourizeMatches(current, prev_to_this, "", "", `${i}`);
-        } else {
-            if (prev_to_this.length > next_from_this.length) {
-                console.log(1);
-                const parts = current.split(prev_to_this);
-                for (let j = 0; j < parts.length; j++) {
-                    // list.push(<span key={`${i}-${j}`}>{parts[j]}</span>);
-                    list = list.concat(colourizeMatches(parts[j], next_from_this, "", "new", `${i}-${j}`));
-                    if (j < parts.length - 1) {
-                        // list.push(<span className="old" key={`${i}-${j}-old`}>{next_from}</span>);
-                        list = list.concat(colourizeMatches(prev_to_this, next_from_this, "new", "new old", `${i}-${j}-new`))
-                    }
+            const parts = current.split(rc_to);
+            for (let j = 0; j < parts.length; j++) {
+                list.push(<span key={`${i}-${j}`}>{parts[j]}</span>);
+                if (j < parts.length - 1) {
+                    list.push(<span className="new" key={`${i}-${j}-new`}>{rc_to}</span>);
                 }
-            } else {
-                console.log(2);
-                const parts = current.split(next_from_this);
-                for (let j = 0; j < parts.length; j++) {
-                    // list.push(<span key={`${i}-${j}`}>{parts[j]}</span>);
-                    list = list.concat(colourizeMatches(parts[j], prev_to_this, "", "old", `${i}-${j}`));
-                    if (j < parts.length - 1) {
-                        // list.push(<span className="old" key={`${i}-${j}-old`}>{next_from}</span>);
-                        list = list.concat(colourizeMatches(next_from_this, prev_to_this, "old", "new old", `${i}-${j}-new`))
-                    }
+            }
+        } else {
+            const parts = current.split(next_from);
+            for (let j = 0; j < parts.length; j++) {
+                list.push(<span key={`${i}-${j}`}>{parts[j]}</span>);
+                if (j < parts.length - 1) {
+                    list.push(<span className="old" key={`${i}-${j}-old`}>{next_from}</span>);
                 }
             }
         }
-        astLIs.push(<li className='expr_history' key={i}><pre>{list}</pre></li>);
+
+        astLIs.push(<li className='expr_history' key={i}>{list}<br /></li>);
     }
-    astLIs.push(<li className='expr_history' key={0}>{astStrings[0]}</li>);
     // astLIs = astLIs.reverse();   
 
     return (
