a003cc9f7db51bf0cf920ae769dd8083f9a77815
Kiran <kiranisturt@outlook.com>
2025-04-15 17:41:00 +0100

    submission build fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://functional.kiransturt.co.uk",
+  base: "/", //"https://functional.kiransturt.co.uk",
   server: {
     fs: {
       strict: false
